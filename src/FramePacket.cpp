1a3f7d285db8cef257f0c9f3ede2d374a63af022
ZhouKai <gyxxzk@gmail.com>
2020-10-08 18:49:58 +0800

    add arc folder
    Signed-off-by: ZhouKai <gyxxzk@gmail.com>

---
+++ src/FramePacket.cpp
@@ -0,0 +1,54 @@
+#include "FramePacket.h"
+
+framePacket::framePacket(){ }
+
+
+framePacket::framePacket(framePacket* packet){
+    this->width_c = packet->width_c;
+    this->height_c = packet->height_c;
+    this->width_d = packet->width_d;
+    this->height_d = packet->height_d;
+
+    this->data_c = new unsigned char[4 * packet->width_c * packet->height_c];
+    this->data_d = new float[packet->width_d * packet->height_d];
+    this->vertices = new Point3fRGB[packet->width_d * packet->height_d];
+
+    this->timestamp_c = packet->timestamp_c;
+    this->timestamp_d = packet->timestamp_d;
+
+    memcpy(this->data_c, packet->data_c, 4 * this->width_c * this->height_c);
+    memcpy(this->data_d, packet->data_d, sizeof(float) * this->width_d * this->height_d);
+    memcpy(this->vertices, packet->vertices, sizeof(Point3fRGB) * this->width_d * this->height_d);
+}
+
+
+void framePacket::init(libfreenect2::Frame *color, 
+                       libfreenect2::Frame *depth, 
+                       Point3fRGB *verts,
+                       int width_c, 
+                       int height_c,
+                       int width_d,
+                       int height_d)
+{
+    this->width_c = width_c;
+    this->height_c = height_c;
+    this->width_d = width_d;
+    this->height_d = height_d;
+
+    this->data_c = new unsigned char[4 * this->width_c * this->height_c];
+    this->data_d = new float[this->width_d * this->height_d];
+
+    this->timestamp_c = color->timestamp;
+    this->timestamp_d = depth->timestamp;
+
+    memcpy(this->data_c, color->data, 4 * this->width_c * this->height_c);
+    memcpy(this->data_d, depth->data, sizeof(float) * this->width_d * this->height_d);
+    this->vertices = verts;
+}
+
+
+void framePacket::destroy(){
+    delete [] this->data_c;
+    delete [] this->data_d;
+    delete [] this->vertices;
+}
---
+++ src/utils.cpp
@@ -0,0 +1,127 @@
+#include "utils.h"
+
+void savePacket2Bin(framePacket *input, const char* filename){
+    std::ofstream binFile;
+    binFile.open(filename, std::ios::out |  std::ios::trunc | std::ios::binary);
+
+    float tmpFloat;
+    char tmpChar;
+
+    for(int i=0; i<input->width_d * input->height_d; i++){
+        tmpFloat = static_cast<float>(input->data_d[i]);
+        binFile.write(reinterpret_cast<const char*>(&tmpFloat), sizeof(float));
+    }
+
+    for(int i=0; i<input->width_d * input->height_d; i++){
+        tmpFloat = static_cast<float>(input->vertices[i].X);
+        binFile.write(reinterpret_cast<const char*>(&tmpFloat), sizeof(float));
+        tmpFloat = static_cast<float>(input->vertices[i].Y);
+        binFile.write(reinterpret_cast<const char*>(&tmpFloat), sizeof(float));
+        tmpFloat = static_cast<float>(input->vertices[i].Z);
+        binFile.write(reinterpret_cast<const char*>(&tmpFloat), sizeof(float));
+        tmpChar = static_cast<char>(input->vertices[i].R);
+        binFile.write(reinterpret_cast<const char*>(&tmpChar),sizeof(char));
+        tmpChar = static_cast<char>(input->vertices[i].G);
+        binFile.write(reinterpret_cast<const char*>(&tmpChar),sizeof(char));
+        tmpChar = static_cast<char>(input->vertices[i].B);
+        binFile.write(reinterpret_cast<const char*>(&tmpChar),sizeof(char));
+    }
+
+    binFile.close();
+}
+
+
+bool saveRGBDFIFO2Image(FIFO<framePacket> **input, int numOfKinects){
+    cv::Mat color, depth;
+    
+    int *cnt = new int[numOfKinects];
+    for(int i=0; i<numOfKinects; i++){
+        cnt[i] = 0;
+    }
+
+    for(int i=0; i<numOfKinects; i++){
+        while(cnt[i]++ < framemax){
+            framePacket *packet = input[i]->get();
+            if( packet == NULL ) {
+                break;
+            }
+            cv::Mat(packet->height_c, packet->width_c, CV_8UC4, packet->data_c).copyTo(color);
+            cv::Mat(packet->height_d, packet->width_d, CV_32FC1, packet->data_d).copyTo(depth);
+
+            cv::Mat point(packet->height_d, packet->width_d, CV_32FC3);
+            cv::Mat RGB(packet->height_d, packet->width_d, CV_8UC3);
+
+            for(int i=0; i<packet->height_d; i++){
+                for(int j=0; j<packet->width_d; j++){
+                    //std::printf("%d %d\n", i, j);
+                    point.at<cv::Vec3f>(i, j)[0] = packet->vertices[i*packet->width_d + j].X;
+                    point.at<cv::Vec3f>(i, j)[1] = packet->vertices[i*packet->width_d + j].Y;
+                    point.at<cv::Vec3f>(i, j)[2] = packet->vertices[i*packet->width_d + j].Z;
+                    // std::printf("i: %d , j: %d | %f %f %f\n", i, j, point.at<cv::Vec3f>(i, j)[0],
+                    //  point.at<cv::Vec3f>(i, j)[1], point.at<cv::Vec3f>(i, j)[2]);
+                    RGB.at<cv::Vec3b>(i, j)[0] = packet->vertices[i*packet->width_d + j].B;
+                    RGB.at<cv::Vec3b>(i, j)[1] = packet->vertices[i*packet->width_d + j].G;
+                    RGB.at<cv::Vec3b>(i, j)[2] = packet->vertices[i*packet->width_d + j].R;
+                }
+            }
+            //cv::normalize(point_save, point_save, 0, 1.0, cv::NORM_MINMAX);
+
+            cv::imwrite("./datas/color" + std::to_string(i) + std::to_string(cnt[i]) + ".png", color);
+            cv::imwrite("./datas/depth" + std::to_string(i) + std::to_string(cnt[i]) + ".png", depth);
+            cv::imwrite("./datas/point" + std::to_string(i) + std::to_string(cnt[i]) + ".tif", point);
+            //cv::ImwriteFlags;
+            cv::imwrite("./datas/rgb" + std::to_string(i) + std::to_string(cnt[i]) + ".png", RGB);
+
+            packet->destroy();
+        }
+    }
+    return true;
+}
+
+
+bool saveRGBDFIFO2PLY(FIFO<framePacket> **input, int numOfKinects){
+    int *cnt = new int[numOfKinects];
+    for(int i=0; i<numOfKinects; i++){
+        cnt[i] = 0;
+    }
+
+    std::vector<Point3f> vertices;
+	std::vector<RGB> colors;
+	RGB tempColor;
+    Point3f temp;
+
+    for(int i=0; i<numOfKinects; i++){
+        while(cnt[i]++ < framemax){
+            framePacket *packet = input[i]->get();
+            if( packet == NULL ) {
+                break;
+            }
+
+            cv::Mat point(packet->height_d, packet->width_d, CV_32FC3);
+            cv::Mat RGB(packet->height_d, packet->width_d, CV_8UC3);
+
+            for(int i=0; i<packet->height_d; i++){
+                for(int j=0; j<packet->width_d; j++){
+                    //std::printf("%d %d\n", i, j);
+                    temp.X = packet->vertices[i*packet->width_d + j].X;
+                    temp.Y = packet->vertices[i*packet->width_d + j].Y;
+                    temp.Z = packet->vertices[i*packet->width_d + j].Z;
+                    // std::printf("i: %d , j: %d | %f %f %f\n", i, j, point.at<cv::Vec3f>(i, j)[0],
+                    //  point.at<cv::Vec3f>(i, j)[1], point.at<cv::Vec3f>(i, j)[2]);
+                    tempColor.B = packet->vertices[i*packet->width_d + j].B;
+                    tempColor.G = packet->vertices[i*packet->width_d + j].G;
+                    tempColor.R = packet->vertices[i*packet->width_d + j].R;
+
+                    vertices.push_back(temp);
+                    colors.push_back(tempColor);
+                }
+            }
+            std::string filename = "./datas/pointcloud_" + std::to_string(i) + "_" + std::to_string(cnt[i]) + ".ply";
+            savePlyFile(filename.c_str(), vertices, false, colors);
+            vertices.clear();
+            colors.clear();
+            packet->destroy();
+        }
+    }
+    return true;
+}
